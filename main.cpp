ccfd1b6dd4cc90d31904db4c08677a05da1975ee
Psiker <55mehmetokur@gmail.com>
2025-04-15 14:40:38 +0300

    s
    s

--- main.cpp
+++ main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    //selam herkese 
+    double num1, num2;
+    char op;
+
+    cout << "Birinci sayiyi girin: ";
+    cin >> num1;
+
+    cout << "Islem s (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Ikinci sayiyi girin: ";
+    cin >> num2;
+
+    double sonuc;
+
+    switch(op) {
+        case '+':
+            sonuc = num1 + num2;
+            break;
+        case '-':
+            sonuc = num1 - num2;
+            break;
+        case '*':
+            sonuc = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                sonuc = num1 / num2;
+            else {
+                cout << "Hata: 0'a bolme yapilamaz!" << endl;
+                return 1;
+            }
+            break;
+        default:
+            cout << "Gecersiz islem!" << endl;
+            return 1;
+    }
+
+    cout << "Sonuc: " << sonuc << endl;
+
+    cout << "Devam etmek icin bir tusa basin..."; // Kullanıcıya bilgi ver
+    cin.ignore(); // Önceki girdiyi temizle
+    cin.get();
     return 0;
 }
