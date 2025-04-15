e27876eb3c850bc798e1507e43a5b1bf5bc0e384
Mehmet Mert Kahramanoğlu <mehmetmertkahramanoglu@gmail.com>
2025-04-15 15:03:03 +0300

    son hali
    gerekli düzenlemeler yapıldı hatalar silindi(2. main fonksiyonu silindi)

--- main.cpp
+++ main.cpp
@@ -37,45 +37,6 @@ int main() {
             std::cout << "Gecersiz islem!" << std::endl;
             return 1;
     }
-#include <iostream>
-
-int main() {
-    double sayi1, sayi2;
-    char islem;
-
-    std::cout << "Birinci sayiyi girin: ";
-    std::cin >> sayi1;
-
-    std::cout << "Islem secin (+, -, *, /): ";
-    std::cin >> islem;
-
-    std::cout << "Ikinci sayiyi girin: ";
-    std::cin >> sayi2;
-
-    double sonuc;
-
-    switch (islem) {
-        case '+':
-            sonuc = sayi1 + sayi2;
-            break;
-        case '-':
-            sonuc = sayi1 - sayi2;
-            break;
-        case '*':
-            sonuc = sayi1 * sayi2;
-            break;
-        case '/':
-            if (sayi2 != 0)
-                sonuc = sayi1 / sayi2;
-            else {
-                std::cout << "Sifira bolme hatasi!" << std::endl;
-                return 1;
-            }
-            break;
-        default:
-            std::cout << "Gecersiz islem!" << std::endl;
-            return 1;
-    }
 
     std::cout << "Sonuc: " << sonuc << std::endl;
     std::cin.get(); 
